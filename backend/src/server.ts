a85db33ba1e044d9fa0f4a8fac9f2e7697918fcd
Vinicius <marcussampaio123@hotmail.com>
2023-09-16 12:56:00 -0300

    adding new architecture for backend

--- backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const PORT = process.env.PORT || 3333;
-
-app.listen(PORT, () => console.log("Server running or port 3333"));
-
