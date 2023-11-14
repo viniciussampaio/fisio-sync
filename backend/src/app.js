a583b571cbe40c8cdcc69d523c80cd08fefb4745
Vinicius <marcussampaio123@hotmail.com>
2023-11-14 11:46:31 -0300

    finish part one project

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,11 @@ const routeSchedules = require("./routes/schedules");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-const app = express();
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const app = express();
+
 app.use(express.json());
 app.use(routeSchedules);
 
