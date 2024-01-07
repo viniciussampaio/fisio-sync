7c5e880970a178456e13e0b158b556ae1a567583
Vinicius <marcussampaio123@hotmail.com>
2024-01-07 10:31:18 -0300

    upload backend changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(bodyParser.urlencoded({ extended: true }));
 const app = express();
 
 app.use(express.json());
